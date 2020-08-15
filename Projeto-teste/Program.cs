205a2f1a37ef9085e4a4ff3122122a0db670fdd9
Takashi Arai <takashi.arai2018@gmail.com>
2020-08-15 13:01:05 -0300

    Acrescentada uma mensagem de Bom Dia!

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -7,6 +7,9 @@ namespace Projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia!");
+
+            Console.ReadLine();
         }
     }
 }
